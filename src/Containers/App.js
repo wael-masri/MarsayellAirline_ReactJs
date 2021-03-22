36f5e2031ec8e46f1b8f65848157c581798a9412
wael-masri <80285647+wael-masri@users.noreply.github.com>
2021-03-23 00:04:06 +0200

    Update App.js

--- src/Containers/App.js
+++ src/Containers/App.js
@@ -31,7 +31,7 @@ class  App extends Component {
           <Iconscrollup />
          
           <Route exact path='/' component={Home} />
-          <Route exact path='/MarsayellAirline_ReactJs' component={Home} />
+          <Route exact path='/MarsayellAirline_ReactJs/' component={Home} />
         
           <Route path='/About' component={About} />
           <Route path='/Gallery' component={Gallery} />
